2df5a192438be52bb28db1dffce9bb84971cecf0
Zhiznevski <w.heisenberggg@gmail.com>
2024-02-11 23:59:18 +0300

    feat: add VideoPlayer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import MobileMenu from './components/MobileMenu/MobileMenu';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 import VideosFeed from './components/VideosFeed/VideosFeed';
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
       <Header/>
       <main className='main'>
       <div class="main-container container">
+        <div className='main-content'>
+          <VideoPlayer/>
+        </div>
         <VideosFeed/>
       </div>
       </main>
