df44fafaf708ed2cc80dba3fa9c671e719a24714
Zhiznevski <w.heisenberggg@gmail.com>
2024-02-11 14:55:13 +0300

    feat: create Header component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <main className='main'>
+      <div class="main-container container">
+      </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+export const videos = [
+    {
+      id: "1",
+      title: "Baby Monitor Technology",
+      views: "123k",
+      author: "Dollie Blair",
+      duration: "8:00",
+      videoUrl: "./assets/images/video1.png",
+    },
+    {
+        id: "2",
+        title: "A Good Autoresponder",
+        views: "123k",
+        author: "Dollie Blair",
+        duration: "8:00",
+        videoUrl: "./assets/images/video2.png",
+      },
+      {
+        id: "3",
+        title: "Selecting The Right Hotel",
+        views: "123k",
+        author: "Dollie Blair",
+        duration: "8:00",
+        videoUrl: "./assets/images/video3.png",
+      },
+      {
+        id: "4",
+        title: "Selecting The Right Hotel",
+        views: "123k",
+        author: "Dollie Blair",
+        duration: "8:00",
+        videoUrl: "./assets/images/video4.png",
+      },
+]
